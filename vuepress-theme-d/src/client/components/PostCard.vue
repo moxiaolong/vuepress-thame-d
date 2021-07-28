b00a48b4babb8e76371bafa59cfc149df53a26d0
dragon <975425198@qq.com>
2021-07-28 16:12:58 +0800

    first commit

---
+++ vuepress-theme-d/src/client/components/PostCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    postcard
+    {{ post.title }}
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType} from 'vue'
+import type {Post} from "../../shared";
+
+export default defineComponent({
+
+      name: "PostCard",
+      props: {
+        post: { // 菜单数据
+          type: Object as PropType<Post>,
+          required: true
+        }
+      },
+
+      setup(props) {
+
+        let post = props.post;
+
+        return {
+          post
+        }
+      }
+    }
+)
+
+</script>
---
+++ vuepress-theme-d/src/node/defaultTheme.ts
@@ -0,0 +1,109 @@
+import type {Theme, ThemeConfig} from '@vuepress/core'
+import {path} from '@vuepress/utils'
+import type {
+    DefaultThemeLocaleOptions,
+    DefaultThemePluginsOptions,
+} from '../shared'
+import {
+    assignDefaultLocaleOptions,
+    resolveActiveHeaderLinksPluginOptions,
+    resolveContainerPluginOptions,
+    resolveContainerPluginOptionsForDetails,
+    resolveContainerPluginOptionsForCodeGroup,
+    resolveContainerPluginOptionsForCodeGroupItem,
+    resolveGitPluginOptions,
+    resolveMediumZoomPluginOptions,
+} from './utils'
+import {Post} from "../shared/post";
+
+export interface DefaultThemeOptions
+    extends ThemeConfig,
+        DefaultThemeLocaleOptions {
+    /**
+     * To avoid confusion with the root `plugins` option,
+     * we use `themePlugins`
+     */
+    themePlugins?: DefaultThemePluginsOptions
+}
+
+export const defaultTheme: Theme<DefaultThemeOptions> = ({
+                                                             themePlugins = {},
+                                                             ...localeOptions
+                                                         }) => {
+    assignDefaultLocaleOptions(localeOptions)
+
+    //存放文章列表
+    let posts: Post[] = []
+
+    return {
+        //页面信息回调 获取文章列表
+        extendsPageData: (page, app) => {
+            // && page.date !== "0000-00-00"
+            if (page.filePathRelative != null && page.filePathRelative.endsWith(".md")) {
+                posts.push({
+                    date: page.date,
+                    title: page.title,
+                    excerpt: page.excerpt,
+                    path: page.path
+                })
+            }
+        },
+
+        //定义常量 可以在客户端使用
+        define: {
+            __POSTS__: posts
+        },
+
+        name: '@vuepress/theme-default',
+
+        layouts: path.resolve(__dirname, '../client/layouts'),
+
+        clientAppEnhanceFiles: path.resolve(
+            __dirname,
+            '../client/clientAppEnhance.js'
+        ),
+
+        clientAppSetupFiles: path.resolve(__dirname, '../client/clientAppSetup.js'),
+
+        // // use the relative file path to generate edit link
+        // extendsPageData: ({filePathRelative}) => ({filePathRelative}),
+
+        plugins: [
+            [
+                '@vuepress/active-header-links',
+                resolveActiveHeaderLinksPluginOptions(themePlugins),
+            ],
+            ['@vuepress/back-to-top', themePlugins.backToTop !== false],
+            [
+                '@vuepress/container',
+                resolveContainerPluginOptions(themePlugins, localeOptions, 'tip'),
+            ],
+            [
+                '@vuepress/container',
+                resolveContainerPluginOptions(themePlugins, localeOptions, 'warning'),
+            ],
+            [
+                '@vuepress/container',
+                resolveContainerPluginOptions(themePlugins, localeOptions, 'danger'),
+            ],
+            [
+                '@vuepress/container',
+                resolveContainerPluginOptionsForDetails(themePlugins),
+            ],
+            [
+                '@vuepress/container',
+                resolveContainerPluginOptionsForCodeGroup(themePlugins),
+            ],
+            [
+                '@vuepress/container',
+                resolveContainerPluginOptionsForCodeGroupItem(themePlugins),
+            ],
+            ['@vuepress/git', resolveGitPluginOptions(themePlugins, localeOptions)],
+            ['@vuepress/medium-zoom', resolveMediumZoomPluginOptions(themePlugins)],
+            ['@vuepress/nprogress', themePlugins.nprogress !== false],
+            ['@vuepress/palette', {preset: 'sass'}],
+            ['@vuepress/prismjs', themePlugins.prismjs !== false],
+            ['@vuepress/theme-data', {themeData: localeOptions}],
+        ],
+    }
+}
